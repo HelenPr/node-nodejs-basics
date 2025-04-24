533857bf14059188a9a52d4e747a757a0ee71ea4
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-04-24 21:05:14 +0400

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { open } from 'node:fs/promises';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.resolve('src/fs/files', 'fresh.txt');
+
+  try {
+    const fileHandle = await open(filePath, 'wx');
+    const content = 'I am fresh and young';
+    try {
+      await fileHandle.writeFile(content, 'utf-8');
+    } finally {
+      await fileHandle.close();
+    }
+  } catch (err) {
+    const errorMessage = 'FS operation failed';
+    if (err.code === 'EEXIST') {
+      throw new Error(errorMessage);
+    }
+    throw err;
+  }
 };
 
 await create();
