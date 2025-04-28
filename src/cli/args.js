f3d67223171ad840ca2af7466ff90a9cd3f3b6b9
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-04-29 02:40:37 +0400

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+      const key = args[i].replace('--', '');
+      const value = args[i + 1];
+      parsedArgs[key] = value;
+  }
+
+  for (const [key, value] of Object.entries(parsedArgs)) {
+      console.log(`${key} is ${value}`);
+  }
 };
 
 parseArgs();
