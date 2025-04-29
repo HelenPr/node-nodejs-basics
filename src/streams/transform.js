1f465bb136a57482439c8c264d455d832f56925b
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-04-29 17:32:48 +0400

    feat: implement transform function using stream api

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        const reversedChunk = chunk.toString().trim().split('').reverse().join('');
+        callback(null, reversedChunk);
+    }
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
+
+  reverseTransform.on('error', (err) => {
+    console.error(`Error in the stream: ${err.message}`);
+  });
 };
 
 await transform();
