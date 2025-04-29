1f08937d624252335a8ca745c00ad5f875ff2dc0
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-04-29 22:47:21 +0400

    feat: execute computation with worker threads api

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { cpus } from 'node:os';
+import path from 'node:path';
+import { Worker } from 'node:worker_threads';
+
+const numCPUs = cpus().length;
+const workerPath = path.resolve(import.meta.dirname, 'worker.js');
+const initialData = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = Array.from({ length: numCPUs }, (_, i) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(workerPath, {
+        workerData: initialData + i,
+      });
+
+      worker.on('message', (msg) => {
+        resolve(msg);
+      });
+
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+      });
+    });
+  });
+
+  const result = await Promise.all(workers);
+  console.log(result);
 };
 
 await performCalculations();
