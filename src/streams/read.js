1e96bfd3cab75d26973dc96d60e6f39a91e0d96b
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-04-29 04:19:22 +0400

    feat: implement read function using stream api

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import { createReadStream } from 'fs';
+import path from 'node:path';
+import readline from 'readline';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(import.meta.dirname, 'files', 'fileToRead.txt');
+  const stream = createReadStream(filePath);
+
+  stream.pipe(process.stdout);
+
+  await new Promise((resolve, reject) => {
+    stream.on('end', () => resolve());
+    stream.on('error', (error) => reject(error));
+  });
+
+  console.log('\n\nPress Enter to exit...');
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  await new Promise((resolve) => {
+    rl.question('', () => {
+        rl.close();
+        resolve();
+    });
+  });
 };
 
 await read();
