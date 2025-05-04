6730c709e39c9db6975b9c24d68a5f3bb4c88c9a
LAPTOP-MPGEDP13\0fake <0fake0lake0@gmail.com>
2025-05-05 03:02:24 +0400

    feat: implement up function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import readline from 'node:readline';
 import process from 'node:process';
 import os from 'node:os';
 
+import { up } from './src/commands/up.js'
+
 const DEFAULT_USERNAME = 'User';
 const EXIT_CMD = '.exit';
 
@@ -15,22 +17,36 @@ const printCWD = () => {
 
 printCWD();
 
+const commandsMap = {
+  up
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const commandHandler = (command) => {
-  const trimmed = command.trim();
+const commandHandler = (commandLine) => {
+  const trimmed = commandLine.trim();
   try {
     if (trimmed === EXIT_CMD) {
       console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
       process.exit(0);
-    } else {
+    } else if (!trimmed){
       console.log('Invalid input');
+    } else {
+      const [command, ...args] = trimmed.split(' ');
+      const handler = commandsMap[command];
+      if (handler) {
+        handler(args);
+      } else {
+        console.log('Invalid input');
+      }
     }
   } catch(error) {
     console.log('Operation failed');
+  } finally {
+    printCWD();
   }
 };
 
